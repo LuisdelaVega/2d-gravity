8559f68627728360080e3ab07db6003d6e3294d9
Luis R. de la Vega Lopez <luisr.delavega@gmail.com>
2020-10-23 19:40:00 -0400

    Initial commit

---
+++ Assets/GravityBody.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GravityBody : MonoBehaviour
+{
+    public Transform planet;
+
+    public float maxGravity;
+    public float maxGravityDistance;
+
+    private Transform m_transform;
+    private Rigidbody2D rb; // Player's Rigidbody2D
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        m_transform = transform;
+    }
+
+    void FixedUpdate()
+    {
+        float distance = Vector2.Distance(planet.position, m_transform.position);
+        Vector3 v = planet.position - m_transform.position;
+        rb.AddForce(v.normalized * (1.0f - distance / maxGravityDistance) * maxGravity);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Controls controls;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        controls = new Controls();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+        controls.Player.Movement.performed += ctx => SetDirection(ctx.ReadValue<Vector2>());
+    }
+    private void OnDisable() => controls.Disable();
+
+    private void FixedUpdate()
+    {
+        if (direction.sqrMagnitude != 0)
+            rb.AddForce(speed * direction);
+    }
+
+    private void SetDirection(Vector2 dir) => direction = dir;
+}
